83ebcffb9ae1126a6995240da9cb74b6f169d057
M. Isuru Tharanga Chrishantha Perera <chrishantha@gmail.com>
2017-12-13 15:17:30 +0530

    Update Ballerina to 0.95.4. Added more headers to understand the environment details and get the timing measurements for VM startup time, handler initializtion time and request processing time

--- src/main/java/com/github/chrishantha/lambda/ballerina/BallerinaRequestHandler.java
+++ src/main/java/com/github/chrishantha/lambda/ballerina/BallerinaRequestHandler.java
@@ -14,25 +14,46 @@ import org.ballerinalang.net.http.HttpDispatcher;
 import org.ballerinalang.net.http.HttpResource;
 import org.wso2.transport.http.netty.message.HTTPCarbonMessage;
 
+import java.lang.management.ManagementFactory;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class BallerinaRequestHandler implements RequestHandler<Request, Response> {
 
     private static final String BAL_PACKAGE_ENV_VAR = "BAL_PACKAGE";
     private static final String BAL_SERVICE_PATH_ENV_VAR = "BAL_SERVICE_PATH";
     private static final String BAL_SERVICE_METHOD_ENV_VAR = "BAL_SERVICE_METHOD";
-    private static final String HEADER_FUNCTION_NAME = "x-bal-lambda-function-name";
-    private static final String HEADER_FUNCTION_VERSION = "x-bal-lambda-function-version";
-    private static final String HEADER_REMAINING_TIME_IN_MILLIS = "x-bal-lambda-remaining-time-in-millis";
+    private static final String HEADER_FUNCTION_NAME = "x-function-name";
+    private static final String HEADER_FUNCTION_VERSION = "x-function-version";
+    private static final String HEADER_REMAINING_TIME_IN_MILLIS = "x-remaining-time-in-millis";
+    private static final String HEADER_UP_TIME = "x-up-time-in-millis";
+    private static final String HEADER_VM_STARTUP_TIME = "x-vm-startup-time-in-millis";
+    private static final String HEADER_HANDLER_INIT_TIME = "x-handler-init-time-in-millis";
+    private static final String HEADER_PROCESSING_TIME = "x-processing-time-in-millis";
+    private static final String HEADER_OS_NAME = "x-os-name";
+    private static final String HEADER_OS_ARCH = "x-os-arch";
+    private static final String HEADER_OS_VERSION = "x-os-version";
+    private static final String HEADER_AVAILABLE_PROCESSORS = "x-available-processors";
+    private static final String HEADER_FREE_MEMORY = "x-free-memory-in-bytes";
+    private static final String HEADER_MAX_MEMORY = "x-max-memory-in-bytes";
+    private static final String HEADER_TOTAL_MEMORY = "x-total-memory-in-bytes";
 
     private final CompileResult compileResult;
     private final String BAL_PACKAGE;
     private final String BAL_SERVICE_PATH;
     private final String BAL_SERVICE_METHOD;
 
+    private final long vmStartupTime;
+    private final long handlerInitTime;
+
     public BallerinaRequestHandler() {
+        long startNanoTime = System.nanoTime();
+        long startTime = System.currentTimeMillis();
+        long vmStartTime = ManagementFactory.getRuntimeMXBean().getStartTime();
+        vmStartupTime = startTime - vmStartTime;
+        System.out.println(String.format("VM Startup Time: %d ms", vmStartupTime));
         System.setProperty("java.util.logging.manager", "org.ballerinalang.logging.BLogManager");
         // Check for BAL_FILE_NAME environment variable
         BAL_PACKAGE = getEnvVar(BAL_PACKAGE_ENV_VAR, "echoService");
@@ -43,7 +64,9 @@ public class BallerinaRequestHandler implements RequestHandler<Request, Response
             throw new RuntimeException("There are errors in the Ballerina Service");
         }
         BServiceUtil.runService(compileResult);
-        System.out.println("Request Handler Initialized.");
+        handlerInitTime = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNanoTime);
+        System.out.println(String.format("Request Handler Initialized. Initialization Time %d ms",
+                handlerInitTime));
     }
 
     private String getEnvVar(String name, String def) {
@@ -52,17 +75,30 @@ public class BallerinaRequestHandler implements RequestHandler<Request, Response
     }
 
     public Response handleRequest(Request request, Context context) {
+        long startNanoTime = System.nanoTime();
         LambdaLogger logger = context.getLogger();
         logger.log(String.format("Received : %s", request.getBody()));
         String responseBody = invokeBallerinaService(request.getBody());
         Map<String, String> headers = new HashMap<>();
+        Response response = new Response();
+        response.setHeaders(headers);
+        response.setBody(responseBody);
         // AWS Request ID is already available as a header. See: x-amzn-RequestId
         headers.put(HEADER_FUNCTION_NAME, context.getFunctionName());
         headers.put(HEADER_FUNCTION_VERSION, context.getFunctionVersion());
         headers.put(HEADER_REMAINING_TIME_IN_MILLIS, String.valueOf(context.getRemainingTimeInMillis()));
-        Response response = new Response();
-        response.setHeaders(headers);
-        response.setBody(responseBody);
+        headers.put(HEADER_UP_TIME, String.valueOf(ManagementFactory.getRuntimeMXBean().getUptime()));
+        headers.put(HEADER_VM_STARTUP_TIME, String.valueOf(vmStartupTime));
+        headers.put(HEADER_HANDLER_INIT_TIME, String.valueOf(handlerInitTime));
+        headers.put(HEADER_OS_NAME, System.getProperty("os.name"));
+        headers.put(HEADER_OS_ARCH, System.getProperty("os.arch"));
+        headers.put(HEADER_OS_VERSION, System.getProperty("os.version"));
+        headers.put(HEADER_AVAILABLE_PROCESSORS, String.valueOf(Runtime.getRuntime().availableProcessors()));
+        headers.put(HEADER_FREE_MEMORY, String.valueOf(Runtime.getRuntime().freeMemory()));
+        headers.put(HEADER_MAX_MEMORY, String.valueOf(Runtime.getRuntime().maxMemory()));
+        headers.put(HEADER_TOTAL_MEMORY, String.valueOf(Runtime.getRuntime().totalMemory()));
+        long requestProcessingTime = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNanoTime);
+        headers.put(HEADER_PROCESSING_TIME, String.valueOf(requestProcessingTime));
         return response;
     }
 
--- src/test/java/BallerinaHandlerTest.java
+++ src/test/java/BallerinaHandlerTest.java
@@ -1,14 +1,51 @@
 package com.github.chrishantha.lambda.ballerina;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import org.mockito.Mockito;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static org.mockito.Mockito.when;
+
 public class BallerinaHandlerTest {
+
+    private BallerinaRequestHandler ballerinaRequestHandler;
+
+    @BeforeClass
+    private void initialize() {
+        ballerinaRequestHandler = new BallerinaRequestHandler();
+    }
 
     @Test
     public void echoTest() {
         String payload = "Hello, World!";
-        Assert.assertEquals(new BallerinaRequestHandler().invokeBallerinaService(payload), payload);
+        Assert.assertEquals(ballerinaRequestHandler.invokeBallerinaService(payload), payload);
+    }
+
+    @Test
+    public void handleRequestTest() {
+        String payload = "Hello, World!";
+        Request request = new Request();
+        request.setBody(payload);
+
+        Context context = Mockito.mock(Context.class);
+        when(context.getLogger()).thenReturn(new LambdaLogger() {
+            @Override
+            public void log(String message) {
+                System.out.println(message);
+            }
+
+            @Override
+            public void log(byte[] message) {
+
+            }
+        });
+
+        Response response = ballerinaRequestHandler.handleRequest(request, context);
+        System.out.println(response);
+        Assert.assertEquals(response.getBody(), payload);
     }
 
 }
